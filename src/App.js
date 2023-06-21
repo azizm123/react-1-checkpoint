7e239b16464139536187948b90a8d14d2e216a3b
Mohamed Aziz Mimoun <azizmimoun@Mohameds-MacBook-Pro.local>
2023-06-21 06:49:07 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Price from './components/Price';
+import Photo from './components/Photo';
+import Name from './components/Name' ;
+import Description from './components/Description';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='product' >
+      <Name></Name>
+      <Description id='descript'></Description>
+      <Price id='prix'></Price>
+      <Photo id='ph'></Photo>
+      
     </div>
   );
 }
